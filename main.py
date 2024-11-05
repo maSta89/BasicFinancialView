84be4153c22301e814f4ebbf12b5f4fc53bbc730
maSta89 <hastikj@seznam.cz>
2024-11-05 20:05:49 +0100

    new block added

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
 #import all necessary library
+import yfinance as yf
+import matplotlib.pyplot as plt
 
 # Get the data from online source here
+ticker_symbol = "KO"
+data = yf.download(ticker_symbol, start="2022-01-01", end="2024-01-01")
+
+print(data.head())
+
+closing_prices = data["Close"]
+
+#graph block
+
+plt.figure(figsize=(15,5))
+plt.plot(closing_prices, label="Closing prices")
+plt.title(f"{ticker_symbol} Stock closing prices")
+plt.xlabel("Date")
+plt.ylabel("Prices")
+plt.legend()
+plt.show()
