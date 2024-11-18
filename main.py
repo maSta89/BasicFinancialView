ab2cf99e6a28a20396d65c80ac24cfa60d33c7c9
Jiri Hastik <jiri.hastik@tietoevry.com>
2024-11-18 12:11:30 +0100

    tidy up

--- main.py
+++ main.py
@@ -1,12 +1,37 @@
 # import all necessary library
+import os
 import yfinance as yf
 import matplotlib.pyplot as plt
 import pandas as pd
 from statsmodels.tsa.arima.model import ARIMA
+from datetime import datetime, timedelta
+
+ticker_symbol = "KO"
+open_price_file = "CocaCola_Open_Prices.xlsx"
+close_price_file = "CocaCola_Close_Prices.csv"
+
+# last data in data
+def get_last_date(file_path, date_col):
+    if os.path.exists(file_path):
+        if file_path.endswith(".xlsx"):
+            data = pd.read_excel(file_path, sheet_name="Open Prices")
+        elif file_path.endswith(".csv"):
+            data = pd.read_csv(file_path)
+        last_date = pd.to_datetime(data[date_col].iloc[-1])
+        return last_date
+    return None
+
+
+last_date = get_last_date(open_price_file, "Date")
+if last_date is None:
+    start_date = "2022-01-01"
+else:
+    start_date = (last_date + timedelta(days=1)).strftime("%Y-%m-%d")
 
 # Get the data from online source here
-ticker_symbol = "KO"
-data = yf.download(ticker_symbol, start="2022-01-01", end="2024-01-01", interval="1d")
+
+end_date = "2024-01-01"
+data = yf.download(ticker_symbol, start=start_date, end=end_date, interval="1d")
 data = data.asfreq('D')
 # print(data.columns)
 data[['Open', 'Close']] = data[['Open', 'Close']].ffill()  # missing values added to ensure daily frequency
@@ -21,10 +46,19 @@ forecast = prediction_model.forecast(steps=forecast_steps)
 forecast_index = pd.date_range(start=time_series.index[-1] + pd.Timedelta(days=1), periods=forecast_steps, freq='D')
 
 # split data into separate files
-data = data.reset_index()
-data['Date'] = data['Date'].dt.date
+new_data = data.reset_index()
+new_data['Date'] = new_data['Date'].dt.date
+
+
+
 data[['Date', 'Open']].to_excel("CocaCola_Open_Prices.xlsx", sheet_name="Open Prices")
+
+
+
 data[['Date', 'Close']].to_csv("CocaCola_Close_Prices.csv", index=False)
+
+
+
 
 # graph block
 plt.figure(figsize=(12, 6))
