0dee653a430a5d2e1144ce470187e589da1e3a3f
Jiri Hastik <jiri.hastik@tietoevry.com>
2024-11-28 21:04:36 +0100

    database integration

--- main.py
+++ main.py
@@ -9,29 +9,28 @@ from statsmodels.tsa.arima.model import ARIMA
 from datetime import datetime, timedelta
 
 ticker_symbol = input("Enter ticker symbol like KO for CocaCola").strip().upper()
-open_price_file = f"{ticker_symbol}_Open_Prices.xlsx"
-close_price_file = f"{ticker_symbol}_Close_Prices.csv"
 
-#  set up empty files for data
-if not os.path.exists(open_price_file):
-    pd.DataFrame(columns=["Date", "Open"]).to_excel(open_price_file, sheet_name="Open Prices", index=False)
-
-if not os.path.exists(close_price_file):
-    pd.DataFrame(columns=["Date", "Close"]).to_csv(close_price_file, index=False)
+database_name = f"{ticker_symbol}_stock_data.db"
 
 
-# check for last date in file
-def get_last_date(file_path, date_col):
-    file_data = None
-    if os.path.exists(file_path):
-        if file_path.endswith(".xlsx"):
-            file_data = pd.read_excel(file_path, sheet_name="Open Prices")
-        # Check if the file is not empty
-        if not file_data.empty:
-            bottom_date = pd.to_datetime(file_data[date_col].iloc[-1])
-            return bottom_date
-    return None
+def initialize_database(database):
+    if not os.path.exists(database):
+        print(f"DB file {database} does not exists. Preparing")
+        with sqlite3.connect(database) as connection:
+            cursor = connection.cursor()
+            cursor.execute("""
+            CREATE TABLE IF NOT EXISTS stock_prices (
+                id  INTEGER PRIMARY KEY AUTOINCREMENT,
+                date DATE NOT NULL,
+                open_price REAL,
+                close_price REAL,
+                volume INTEGER
+                )
+            """)
 
+            connection.commit()
+    else:
+        print(f"DB {database} is ready")
 
 # User input for time period
 today = datetime.now().strftime("%Y-%m-%d")
@@ -54,11 +53,6 @@ except ValueError:
 if start_date > end_date:
     raise ValueError("Start date must be earlier than end date.")
 
-last_date = get_last_date(open_price_file, "Date")
-if last_date is None:
-    start_date = default_start  # Default start date if no data exists
-else:
-    start_date = (last_date + timedelta(days=1)).strftime("%Y-%m-%d")  # Start from the day after the last recorded date
 
 # Get the data from online source ,download the data from Yahoo Finance
 data = yf.download(ticker_symbol, start=start_date, end=end_date, interval="1d")
@@ -70,21 +64,6 @@ data['Date'] = pd.to_datetime(data['Date']).dt.date
 print(data)
 print(data.columns)
 
-database_name = f"{ticker_symbol}_stock_data.db"
-connection = sqlite3.connect(database_name)
-cursor = connection.cursor()
-
-cursor.execute("""
-CREATE TABLE IF NOT EXISTS stock_prices (
-    id  INTEGER PRIMARY KEY AUTOINCREMENT,
-    date DATE NOT NULL,
-    open_price REAL,
-    close_price REAL,
-    volume INTEGER
-    )
-""")
-
-connection.commit()
 
 
 def insert_stock_data(rawdata):
