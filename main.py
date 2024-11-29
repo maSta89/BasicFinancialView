08308874fd0a1c8428f2c140d1ddaba912964f28
Jiri Hastik <jiri.hastik@tietoevry.com>
2024-11-29 14:53:34 +0100

    database functions

--- main.py
+++ main.py
@@ -95,21 +95,21 @@ store_yahoo_data(ticker_symbol, start_date, end_date)
 
 
 # prediction block
-csv_data = pd.read_csv(close_price_file, parse_dates=["Date"])
-csv_data.set_index("Date", inplace=True)
-time_series = csv_data['Close']  # base data
-time_series = time_series.asfreq("B")
-prediction_model = ARIMA(time_series, order=(1, 1, 1)).fit()
-forecast_steps = 10  # number of predictions
-forecast = prediction_model.forecast(steps=forecast_steps)
-forecast_index = pd.date_range(start=time_series.index[-1] + pd.Timedelta(days=1), periods=forecast_steps, freq='D')
+# csv_data = pd.read_csv(close_price_file, parse_dates=["Date"])
+# csv_data.set_index("Date", inplace=True)
+# time_series = csv_data['Close']  # base data
+# time_series = time_series.asfreq("B")
+# prediction_model = ARIMA(time_series, order=(1, 1, 1)).fit()
+# forecast_steps = 10  # number of predictions
+# forecast = prediction_model.forecast(steps=forecast_steps)
+# forecast_index = pd.date_range(start=time_series.index[-1] + pd.Timedelta(days=1), periods=forecast_steps, freq='D')
 
 # graph block
-plt.figure(figsize=(12, 6))
-plt.plot(time_series.index, time_series, label="Original Time Series", color="blue")
-plt.plot(forecast_index, forecast, label="Forecasted Prices", color="orange", marker='o')
-plt.xlabel("Date")
-plt.ylabel("Stock Price")
-plt.title("Stock Price Forecast with ARIMA")
-plt.legend()
-plt.show()
+# plt.figure(figsize=(12, 6))
+# plt.plot(time_series.index, time_series, label="Original Time Series", color="blue")
+# plt.plot(forecast_index, forecast, label="Forecasted Prices", color="orange", marker='o')
+# plt.xlabel("Date")
+# plt.ylabel("Stock Price")
+# plt.title("Stock Price Forecast with ARIMA")
+# plt.legend()
+# plt.show()
