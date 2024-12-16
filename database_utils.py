20e980b7db353309524f607afbd46efe1d3753b5
Jiri Hastik <jiri.hastik@tietoevry.com>
2024-12-16 14:56:14 +0100

    keep index of date

--- database_utils.py
+++ database_utils.py
@@ -49,13 +49,17 @@ def store_yahoo_data(database, tickersymbol, startdate, enddate):
             connection.close()
 
         startdate = missing_start_date
+    get_yahoo_data(tickersymbol, startdate, enddate, database)
 
+
+def get_yahoo_data(tickersymbol, startdate, enddate, database):
     # Get the data from online source ,download the data from Yahoo Finance
     data = yf.download(tickersymbol, start=startdate, end=enddate, interval="1d")
     data.columns = data.columns.get_level_values(0)
     data = data.dropna(subset=['Open', 'Close'])
-    data = data.reset_index()
-    data['Date'] = pd.to_datetime(data['Date']).dt.date
+    # data = data.reset_index()
+    # data['Date'] = pd.to_datetime(data['Date']).dt.date
+    data.index = pd.to_datetime(data.index).dt.date
     print(data)
     print(data.columns)
     insert_stock_data(database, data)
@@ -65,9 +69,15 @@ def store_yahoo_data(database, tickersymbol, startdate, enddate):
 def insert_stock_data(database, rawdata):
     with sqlite3.connect(database) as connection:
         cursor = connection.cursor()
-        for _, row in rawdata.iterrows():
+
+        cursor.execute("""SELECT date FROM stock_prices""")
+        existing_dates = {row[0] for row in cursor.fetchall()}
+
+        new_data = rawdata[~rawdata["Date"].isin(existing_dates)]
+
+        for date, row in new_data.iterrows():
             cursor.execute("""
             INSERT INTO stock_prices (date, open_price, close_price, volume)
             VALUES (?, ?, ?, ?)
-            """, (row["Date"], row["Open"], row["Close"], row["Volume"]))
+            """, (date, row["Open"], row["Close"], row["Volume"]))
         connection.commit()
